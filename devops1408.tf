6b56086a9ca83ccfbb88c6c9bb45d9d85b226e60
Anatoly <workmishiz@gmail.com>
2022-03-08 17:56:49 +0300

    Provider Settings

--- devops1408.tf
+++ devops1408.tf
@@ -17,3 +17,31 @@ provider "digitalocean" {
 data "digitalocean_ssh_key" "terraform" {
   name = "terraform"
 }
+
+resource "digitalocean_droplet" "dev1408" {
+  image = "ubuntu-20-04-x64"
+  name = "dev1408"
+  region = "fra1"
+  size = "s-1vcpu-2gb-amd"
+  private_networking = true
+  ssh_keys = [
+    data.digitalocean_ssh_key.terraform.id
+  ]
+
+  connection {
+    host = self.ipv4_address
+    user = "root"
+    type = "ssh"
+    private_key = file(var.pvt_key)
+    timeout = "2m"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "export PATH=$PATH:/usr/bin",
+      # install nginx
+      "sudo apt update",
+      "sudo apt install -y nginx"
+    ]
+  }
+}
